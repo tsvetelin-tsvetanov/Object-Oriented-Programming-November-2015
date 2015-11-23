c4c67ac81c095d2c02f8c7703b01a8f95e7226a9
Tsvetelin <tsvetelintsvetanov94@gmail.com>
2015-11-23 16:03:47 +0200

    03-Exception-Handling-Homework

---
+++ 03.ExceptionHandlingHomework/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                uint i = uint.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Sqrt(i));
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
---
+++ 03.ExceptionHandlingHomework/02.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+            short min = 1;
+            short max = 100;
+            short index = 0;
+            short[] numbers = new short[10];
+            string errorMessage = "Invalid number! The number must be in range [1...100]";
+
+            while(index < 10)
+            {
+                short i;
+                try
+                {
+                    i = ReadNumber(min, max, short.Parse(Console.ReadLine()));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(errorMessage);
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine(errorMessage);
+                    continue;
+                }
+                catch(ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine(errorMessage);
+                    continue;
+                }
+                min = i;
+                numbers[index] = i;
+                index++;
+                errorMessage = "Invalid number! The number must be in range [" + min + "...100]";
+            }
+
+            foreach(short sh in numbers)
+            {
+                Console.Write(sh + " ");
+            }
+        }
+
+        public static short ReadNumber(short min, short max, short i)
+        {
+            if(i > max || i < min)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return i;
+        }
+    }
+}
